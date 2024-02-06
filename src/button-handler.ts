14402f374b6bba6745573af7ea4f0e748069a257
KMMineCube <63953483+KaoushikMurugan@users.noreply.github.com>
2024-02-05 19:04:11 -0800

    Chore: Setup for node.js discord bot

---
+++ src/button-handler.ts
@@ -0,0 +1,10 @@
+import { ButtonInteraction } from 'discord.js';
+/**
+ * Handles button interactions.
+ * @param interaction The interaction to handle.
+ */
+async function handleButton(interaction: ButtonInteraction): Promise<void> {
+    
+}
+
+export { handleButton };
